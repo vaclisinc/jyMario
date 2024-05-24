10e166fa5a1a483b7b02753a076292f0fb83aeb1
vac. <nckusic2022@gmail.com>
2024-05-25 04:57:56 +0800

    mario can jump and move correctly

--- assets/scripts/Mario.ts
+++ assets/scripts/Mario.ts
@@ -1,56 +1,91 @@
-import { GameMgr } from "./GameMgr"; 
+import { GameMgr } from "./GameMgr";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export class Mario extends cc.Component {
-    @property
-    speed: number = 200;
-
-    @property
-    jumpSpeed: number = 500;
-
-    @property(GameMgr)
-    GameMgr: GameMgr = null;
-
     private _hAxis: number = 0;
-    public get horizontalAxis(): number { return this._hAxis }
-
     private _vAxis: number = 0;
-    public get verticalAxis(): number { return this._vAxis }
-
-    private isJumping: boolean = false;
     private isOnGround: boolean = false;
+    private isJumping: boolean = false;
+    private lives: number;
+    private scores: number;
+    private recentTime: number;
+    private idleFrame: cc.SpriteFrame = null;
+    private anim: cc.Animation = null;
+    private rigidBody: cc.RigidBody;
 
+    @property moveSpeed: number = 500;
+    @property jumpSpeed: number = 900;
+    @property initialPosition: cc.Vec3;
+    @property(GameMgr) GameMgr: GameMgr = null;
+    @property(cc.Node) ground = null;
+    @property(cc.Node) livesPoints = null;
+    @property(cc.Node) scorePoints = null;
+    @property(cc.Node) timePoints = null;
 
     onLoad() {
+        this.node.setPosition(cc.v3(223.711, 87.699, 0));
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
         cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
-
         cc.director.getPhysicsManager().enabled = true;
-        // cc.director.getPhysicsManager().gravity = cc.v2(0, -320); // 设置重力
-
+        // cc.director.getPhysicsManager().gravity = cc.v2(0, -320); // 設置重力
     }
 
-    onDestroy() {
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    start() {
+        this.initValue();
+        this.declareElement();
     }
+
 
     update(dt: number) {
-        if (this._hAxis !== 0) { //mario move
-            this.node.x += this._hAxis * this.speed * dt;
+        this.updatePosition();
+        this.decideAnim();
+        if (this.node.y < this.ground.y) {
+            this.anim.play("die");
+            this.loseLife();
         }
-        if (!this.isOnGround) {
-            let velocity = this.node.getComponent(cc.RigidBody).linearVelocity;
-            velocity.y += cc.director.getPhysicsManager().gravity.y * dt;
-            this.node.getComponent(cc.RigidBody).linearVelocity = velocity;
+    }
+
+    decideAnim() {
+        // this.anim.stop();
+        let velocity = this.rigidBody.linearVelocity;
+
+        if (velocity.x === 0 && velocity.y === 0) {
+            this.anim.play("idle");
         }
+        else if (velocity.x > 0 && velocity.y === 0) { //right
+            this.node.scaleX = Math.abs(this.node.scaleX);
+            if (!this.anim.getAnimationState("run").isPlaying)
+                this.anim.play("run");
+        }
+        else if (velocity.x < 0 && velocity.y === 0) { //left this._hAxis
+            this.node.scaleX = -Math.abs(this.node.scaleX);
+            if (!this.anim.getAnimationState("run").isPlaying)
+                this.anim.play("run");
+        }
+
+    }
+
+    updatePosition(){
+        let velocity = this.rigidBody.linearVelocity;
+        if (this._hAxis !== 0) {
+            velocity.x = this._hAxis * this.moveSpeed;
+        } else if (this._hAxis === 0){
+            velocity.x = 0;
+        }
+        this.rigidBody.linearVelocity = velocity;
     }
 
     onBeginContact(contact, selfCollider, otherCollider) {
         if (otherCollider.node.group === 'ground') {
-            this.isOnGround = true;
+            const worldManifold = contact.getWorldManifold();
+            const normal = worldManifold.normal;
+            // 確保接觸點的法線是指向上的
+            if (normal.y < 0) {
+                this.isOnGround = true;
+                this.rigidBody.linearVelocity.y = 0;
+            }
         }
     }
 
@@ -59,24 +94,35 @@ export class Mario extends cc.Component {
             this.isOnGround = false;
         }
     }
-    
+
     onKeyDown(event: cc.Event.EventKeyboard) {
-        switch(event.keyCode) {
+        switch (event.keyCode) {
             case cc.macro.KEY.left:
                 this._hAxis--;
                 break;
             case cc.macro.KEY.right:
                 this._hAxis++;
                 break;
-            case cc.macro.KEY.up:
-                this.jump();
+            case cc.macro.KEY.up: //jump
+                if (this.isOnGround) {
+                    // this._vAxis++;
+                    this.isJumping = true;
+                    this.anim.play("jump");
+                    this.GameMgr.playJump();
+                    let velocity = this.rigidBody.linearVelocity;
+                    velocity.y = this.jumpSpeed;
+                    this.rigidBody.linearVelocity = velocity;
+                    this.isOnGround = false;
+                    this.isJumping = false;
+                }
                 break;
         }
         this._hAxis = clamp(this._hAxis);
+        this._vAxis = clamp(this._vAxis);
     }
 
     onKeyUp(event: cc.Event.EventKeyboard) {
-        switch(event.keyCode) {
+        switch (event.keyCode) {
             case cc.macro.KEY.left:
                 this._hAxis++;
                 break;
@@ -85,26 +131,46 @@ export class Mario extends cc.Component {
                 break;
         }
         this._hAxis = clamp(this._hAxis);
+        this._vAxis = clamp(this._vAxis);
     }
 
-    jump() {
-        // if (this.isJumping) return;
-        // this.isJumping = true;
-
-        // let jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
-        // let jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
-        // let sequence = cc.sequence( jumpUp, jumpDown, cc.callFunc(() => {
-        //     this.isJumping = false;
-        // }) );
-
-        // this.GameMgr.playJump();
-        // this.node.runAction(sequence);
-        if (this.isOnGround) {
-            this.isOnGround = false;
-            this.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, this.jumpSpeed);
+    loseLife() {
+        this.lives--;
+        if (this.lives <= 0) {
+            console.log('Game Over');
+        } else {
+            this.resetPosition();
         }
     }
+
+
+    initValue() {
+        this.initialPosition = this.node.position;
+        this.lives = 3;
+        this.scores = 0;
+        this.recentTime = 300;
+    }
+
+    declareElement() {
+        this.idleFrame = this.getComponent(cc.Sprite).spriteFrame;
+        this.anim = this.getComponent(cc.Animation);
+        this.rigidBody = this.node.getComponent(cc.RigidBody);
+    }
+
+
+    resetPosition() {
+        let rigidBody = this.node.getComponent(cc.RigidBody);
+        rigidBody.enabled = false;
+        this.node.setPosition(this.initialPosition);
+        rigidBody.enabled = true;
+    }
+
+    onDestroy() {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    }
 }
+
 
 function clamp(value: number, a: number = -1, b: number = 1) {
     if (value < a) return a;
